b89e0a38b8b563656962b23873200eab3b41719a
nerdtron123 <mcmatters123@gmail.com>
2015-07-27 17:07:41 -0400

    AddSQLite

--- com/enduniverse/mysql/MySQL.java
+++ com/enduniverse/mysql/MySQL.java
@@ -1,9 +1,13 @@
 package com.enduniverse.mysql;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import org.bukkit.plugin.Plugin;
 
 public class MySQL {
 	
@@ -13,6 +17,9 @@ public class MySQL {
 	private final String password;
 	private final String port;
 	private final String hostname;
+	private boolean lite;
+	private Plugin plugin;
+	private String dbl;
 	
 	public MySQL(String hostname, String port, String database, String username, String password) {
 		this.hostname = hostname;
@@ -21,10 +28,36 @@ public class MySQL {
 		this.user = username;
 		this.password = password;
 		this.connection = null;
+		this.lite = false;
 	}
 	
+	public void openLite(Plugin plugin, String dbl) throws SQLException {
+		this.lite = true;
+		this.plugin = plugin;
+		this.dbl = dbl;
+		if (isConnected()){
+			return;
+		}
+		if (!plugin.getDataFolder().exists()) {
+			plugin.getDataFolder().mkdirs();
+		}
+		File file = new File(plugin.getDataFolder(), dbl);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		connection = DriverManager.getConnection("jdbc:sqlite:" + plugin.getDataFolder().getPath().toString() + "/" + dbl);
+		}
+	
 	public void open() throws SQLException {
-		connection = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/" + this.database,this.user, this.password);
+		if(lite){
+			openLite(plugin, dbl);
+			return;
+		}
+		if(isConnected()) connection = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/" + this.database,this.user, this.password);
 	}
 	
 	public void close() throws SQLException {
@@ -59,3 +92,4 @@ public class MySQL {
 	}
 	
 }
+	
